4b52736aebc4ec2514f0caef10b5faba9915d5c2
DingGe520R <784872490@qq.com>
2018-06-14 18:37:00 +0800

    1

---
+++ first.js
@@ -0,0 +1,62 @@
+
+function bigger(){
+    var img = document.getElementById("img");
+    var font = document.getElementById("p");
+    img .style .width = '150px';
+    img .style .height = '150px';
+    font.style.fontSize="22px";
+}
+
+function smaller(){
+    var img = document.getElementById("img");
+    var font = document.getElementById("p");
+    img .style .width = '130px';
+    img .style .height = '130px';
+    font.style.fontSize="18px";
+}
+
+function bigger1(){
+    var img = document.getElementById("img1");
+    var font = document.getElementById("p1");
+    img .style .width = '150px';
+    img .style .height = '150px';
+    font.style.fontSize="22px";
+}
+
+function smaller1(){
+    var img = document.getElementById("img1");
+    var font = document.getElementById("p1");
+    img .style .width = '130px';
+    img .style .height = '130px';
+    font.style.fontSize="18px";
+}
+function bigger2(){
+    var img = document.getElementById("img2");
+    var font = document.getElementById("p2");
+    img .style .width = '150px';
+    img .style .height = '150px';
+    font.style.fontSize="22px";
+}
+
+function smaller2(){
+    var img = document.getElementById("img2");
+    var font = document.getElementById("p2");
+    img .style .width = '130px';
+    img .style .height = '130px';
+    font.style.fontSize="18px";
+}
+function bigger3(){
+    var img = document.getElementById("img3");
+    var font = document.getElementById("p3");
+    img .style .width = '150px';
+    img .style .height = '150px';
+    font.style.fontSize="22px";
+}
+
+function smaller3(){
+    var img = document.getElementById("img3");
+    var font = document.getElementById("p3");
+    img .style .width = '130px';
+    img .style .height = '130px';
+    font.style.fontSize="18px";
+}
